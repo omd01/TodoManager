54821fc2af517fe95bc50201794fb534fc212825
omd01 <omdahale01@gmail.com>
2024-04-03 20:45:16 -0700

    Some Changes

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include 'mail.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo List Management</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 
